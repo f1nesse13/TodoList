b630b5f1f0535a6396894c8af007c1fc15443e4e
Joe Pell <jpell3130@gmail.com>
2019-07-14 12:08:36 -0400

    misc files added to configure and add our react components

---
+++ app/assets/javascripts/api/test.js
@@ -0,0 +1,37 @@
+$(() => {
+  console.log('hello');
+  $('html').click(() => {
+    // $.ajax({
+    //   method: 'DELETE',
+    //   url: '/api/todos/5'
+    //   // data: { todo: { title: 'test1', body: 'testing', done: false } }
+    // }).then((res, err) => {
+    //   console.log(res);
+    //   console.log(err);
+    // });
+    // $.ajax({
+    //   method: 'GET',
+    //   url: '/api/todos'
+    //   // data: { todo: { title: 'test1', body: 'testing', done: false } }
+    // }).then((res, err) => {
+    //   console.log(res);
+    //   console.log(err);
+    // });
+    // $.ajax({
+    //   method: 'POST',
+    //   url: '/api/todos',
+    //   data: { todo: { title: 'test1', body: 'testing', done: false } }
+    // }).then((res, err) => {
+    //   console.log(res);
+    //   console.log(err);
+    // });
+    // $.ajax({
+    //   method: 'PATCH',
+    //   url: '/api/todos/4',
+    //   data: { todo: { body: 'updated!' } }
+    // }).then((res, err) => {
+    //   console.log(res);
+    //   console.log(err);
+    // });
+  });
+});
