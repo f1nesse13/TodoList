0240c1e2e45c2c4f4ec034bd6861ce7291d4479f
Joe Pell <jpell3130@gmail.com>
2019-07-18 16:05:36 -0400

    small changes to models

--- app/models/step.rb
+++ app/models/step.rb
@@ -2,7 +2,7 @@ class Step < ApplicationRecord
   validates :title, :todo_id, presence: true
   validates :done, inclusion: { in: [true, false] }
 
-  belongs_to :todos,
+  belongs_to :todo,
              foreign_key: :todo_id,
              class_name: "Todo"
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,8 +1,8 @@
 class Todo < ApplicationRecord
   validates :title, presence: true
   validates :done, inclusion: { in: [true, false] }
-end
 
-has_many :steps,
-         foreign_key: :todo_id,
-         class_name: "Step"
+  has_many :steps, dependent: :destroy,
+                   foreign_key: :todo_id,
+                   class_name: "Step"
+end
