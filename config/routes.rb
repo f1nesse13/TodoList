ba251a92031fda29d470b23d8005d29f61829018
Joe Pell <jpell3130@gmail.com>
2019-07-20 12:11:24 -0400

    added routes for sessions and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "users/sessions"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     resources :todos, except: [:new, :edit] do
@@ -7,6 +8,7 @@ Rails.application.routes.draw do
 
     resources :steps, only: [:update, :destroy]
   end
-
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   root to: "static_pages#root"
 end
